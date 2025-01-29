e3997903e64a10b5023fcea12f4c506ebf75689d
aspirantis <fares.mosbakh@gmail.com>
2025-01-29 14:52:30 +0300

    Second home work

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,13 @@
 public class MainClass {
     public int getLocalNumber() {
-        return 17;
+        return 14;
     }
+
+    private int class_number = 50;
+
+    public int getClassNumber() {
+        return class_number;
+    }
+
+
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,12 +4,24 @@ import org.junit.jupiter.api.Assertions;
 public class MainClassTest {
     @Test
     public void getLocalNumber() {
-        MainClass check = new MainClass();
-        if (check.getLocalNumber() == 14) {
+        MainClass checkLocal = new MainClass();
+        if (checkLocal.getLocalNumber() == 14) {
             System.out.println("SUCCESS! getLocalNumber works correctly");
         }
         else {
-            Assertions.fail("ERROR! The method returns a different number, not 14");
+            Assertions.fail("ERROR! The method getLocalNumber returns a different number, not 14");
         }
+    }
+
+    @Test
+    public void getClassNumber() {
+        MainClass checkClassNumber = new MainClass();
+        if (checkClassNumber.getClassNumber() > 45) {
+            System.out.println("SUCCESS! checkClassNumber works correctly");
+        }
+        else {
+            Assertions.fail("ERROR! The method checkClassNumber returns a different number, lower than 45");
+        }
+
     }
 }
