3d0d7eb2e80b7f434d81f03c1ca735679dc7f23a
aspirantis <fares.mosbakh@gmail.com>
2025-01-30 10:06:51 +0300

    Third home work

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,11 +1,17 @@
 public class MainClass {
+
+    private int class_number = 50;
+    private String class_string = "Jello, world";
+
     public int getLocalNumber() {
         return 14;
     }
 
-    private int class_number = 50;
-
     public int getClassNumber() {
         return class_number;
+    }
+
+    public String getClassString() {
+        return class_string;
     }
 }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -2,10 +2,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class MainClassTest {
+
+    MainClass check = new MainClass();
+
     @Test
     public void getLocalNumber() {
-        MainClass checkLocal = new MainClass();
-        if (checkLocal.getLocalNumber() == 14) {
+        if (check.getLocalNumber() == 14) {
             System.out.println("SUCCESS! getLocalNumber works correctly");
         }
         else {
@@ -15,12 +17,21 @@ public class MainClassTest {
 
     @Test
     public void getClassNumber() {
-        MainClass checkClassNumber = new MainClass();
-        if (checkClassNumber.getClassNumber() > 45) {
-            System.out.println("SUCCESS! checkClassNumber works correctly");
+        if (check.getClassNumber() > 45) {
+            System.out.println("SUCCESS! getClassNumber works correctly");
         }
         else {
-            Assertions.fail("ERROR! The method checkClassNumber returns a different number, lower than 45");
+            Assertions.fail("ERROR! The method getClassNumber returns a different number, lower than 45");
+        }
+    }
+
+    @Test
+    public void testGetClassString() {
+        if (check.getClassString().contains("hello") || check.getClassString().contains("Hello")) {
+            System.out.println("SUCCESS! getClassString works correctly");
+        }
+        else {
+            Assertions.fail("ERROR! The method getClassString returns words other than hello or Hello");
         }
     }
 }
