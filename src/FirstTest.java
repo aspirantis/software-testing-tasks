a7a114089883260addf40fc7a10e12a1e1ef1857
aspirantis <fares.mosbakh@gmail.com>
2025-02-05 01:16:59 +0300

    Home work with config

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -25,7 +25,6 @@ public class FirstTest {
         capabilities.setCapability("app", "D:/aspirantis/JavaAppiumAutomation/apks/org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
-
     }
 
     @After
